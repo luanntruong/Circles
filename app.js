071420155d2520faa2faa71d651942cbf82fbfea
Luan Truong <luantruong210@gmail.com>
2018-02-27 20:26:11 -0800

    delete some unnessary files

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+// handle main page request
 app.get("/", function (req, res) {
     res.render("index.ejs");
 });
+
+// handle other requests
 app.get("/*", function(req, res) {
-    res.send("Page not exist");
+    res.send("Page is not exist");
 });
+
+// listen config
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("The server has started");
 });
